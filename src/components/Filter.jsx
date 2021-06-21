eda04b532d7485cd02057b65503c0bc552bffebc
Julia Burkhart <juliaburkhart@julias-mbp.localdomain>
2021-06-21 13:10:51 +0200

    filter Browse-Entries

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -8,38 +8,56 @@ const FilterDiv = styled.div`
 background-color: var(--color-green-mint);
 border-radius: 5px;
 border: solid var(--color-blue-dark) 1px;
-
 height: 191px;
 margin: 20px 0;
+padding: 12px 16px;
 display: flex;
 flex-direction: column;
-justify-content: space-between;
-padding: 12px 16px;
 `
 const StyledButton = styled(Button)`
 width: 160px;
 padding: 5px 10px;
 `
 
-function Filter() {
+const Form = styled.form`
+flex-grow: 1;
+display: flex;
+flex-direction: column;
+justify-content: space-between;
+margin-top: 10px;
+`
+/* eslint-disable react/prop-types */
+function Filter(props) {
+
+
     return (
-        <FilterDiv>
-<h4>Wonach möchtest du filtern?</h4>
-<form>
-<label htmlFor="keyword">Suchbegriff:</label><br />
-  <input type="text" id="keyword" name="keyword" />
-  <br/>
-  <br />
+
+  <FilterDiv>
+    <h4>Wonach möchtest du filtern?</h4>
+
+    <Form onSubmit={props.onSubmit}>
+      <div>
+        <label htmlFor="keyword">Suchbegriff:</label><br />
+        <input type="text" id="keyword" name="keyword" onChange={props.onKeywordChange} />
+      </div>
 
 {/* Hier vielleicht ne GraphQL Abfrage nach allen Kategorien die es gibt und dann 
 für jede Kategorie ein Radiobutton mit Label erstellen */}
-  <p>Kategorie:</p>
-  <input type="radio" id="strength" name="category" value="male" />
-  <label htmlFor="strength">strength</label><br />
- 
-</form>
-<StyledButton><SmallText>Programme filtern</SmallText></StyledButton>
-</FilterDiv>
+
+      <div>
+        <p>Kategorie:</p>
+        <input type="radio" id="strength" name="category" value="strength" />
+        <label htmlFor="strength">strength</label>
+        
+        <input type="radio" id="coordination" name="category" value="coordination" />
+        <label htmlFor="coordination">coordination</label>
+      </div>
+
+      <StyledButton type="submit" id="filter-button"><SmallText>Programme filtern</SmallText></StyledButton>
+
+    </Form>
+
+  </FilterDiv>
         
     )
 }
--- src/pages/Browse.jsx
+++ src/pages/Browse.jsx
@@ -8,7 +8,7 @@ import { useQuery } from "@apollo/client";
 import {GET_ALLPROGRAMS } from "../graphql/queries";
 
 import Filter from "../components/Filter";
-import BrowserEntry from "../components/BrowserEntry";
+ import BrowserEntry from "../components/BrowserEntry";
 import SmallText from "../elements/SmallText";
 import Spinner from "../components/Spinner";
 
@@ -30,25 +30,51 @@ const Button = styled.button`
 
 function Browse () {
 
-const [filterShown, setFilterShown] = useState(false);
+  const [filterShown, setFilterShown] = useState(false);
+  const [keyword, setKeyword] = useState("");
+  const [programs, setPrograms] = useState([])
 
-const {loading, error, data} = useQuery(GET_ALLPROGRAMS, {
-  variables: { limit: 4, offset: 0 }});
-if (loading) return <Spinner />
-if (error) return <p>`Error: ${error.message}`</p>
-console.log(data.allProgram);
+  const {loading, error, data} = useQuery(GET_ALLPROGRAMS, {
+    variables: { limit: 4, offset: 0 }});
+  if (loading) return <Spinner />
+  if (error) return <p>`Error: ${error.message}`</p>
+  if (programs.length === 0 && data) {
+    setPrograms(data.allProgram)
+  }
+
+
+
+function handleKeywordChange(e) {
+  const currentKeyword = e.target.value;
+  setKeyword(currentKeyword);
+}
+
+function handleFilterSubmit(e) {
+  e.preventDefault();
+  console.log("submitted");
+  console.log(keyword);
+  const filteredPrograms = programs.filter((program)=> program.title.toLowerCase().includes(keyword));
+  console.log(filteredPrograms);
+  setPrograms(filteredPrograms);
+}
+
+
+
+
 
 function handleClick() {
 (filterShown === false) ? setFilterShown(true) : setFilterShown(false);
 }
 
+
+
   return (
     <PageDiv>
       <h2>Browse</h2>
       <Button onClick={handleClick}><SmallText>Filter</SmallText></Button>
-{(filterShown === true) && <Filter />}
+{(filterShown === true) && <Filter onSubmit={handleFilterSubmit} onKeywordChange={handleKeywordChange} />}
 
-      {data.allProgram.map(program => {
+      {programs.map(program => {
   return ( 
       <BrowserEntry title={program.title} id={program._id} key={program._id} />
   )
